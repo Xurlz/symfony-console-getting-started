2e4a212a383694774c509c9bf0eaa22d99ede437
Charles Sena <charles.sena.sa@gmail.com>
2026-04-06 08:28:22 -0300

    feat: Add symfony/console use

--- application.php
+++ application.php
@@ -1,7 +1,19 @@
 #!/usr/bin/env php
 <?php
 
-echo '🫖 I\'m a teapot';
+require __DIR__.'/vendor/autoload.php';
 
-exit(1);
+use Symfony\Component\Console\SingleCommandApplication;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputInterface;
+
+
+/* $application->addCommand(new TeapotCommand); */
+(new SingleCommandApplication)
+  ->setCode( function (InputInterface $input, OutputInterface $output) : int {
+    $output->writeln("🫖 I'm a teapot");
+    return Command::FAILURE;
+  })
+  ->run();
 
